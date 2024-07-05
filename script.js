b460c9bfa5e6a3b88e868532609a84444282ed89
Kilian Baier <kilian.baier16@gmail.com>
2024-07-05 13:16:30 +0200

    added calcfunctions

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 const screen = document.getElementById("value-toShow");
 
+
+let value = {
+    firstValue: "",
+    mode: "",
+    secondValue: "",
+
+}
+
 const buttons = document.querySelectorAll("button").forEach((e) => {
     e.addEventListener("click", function() {
-        screen.innerText += e.value;
+        if(e.value === "C"){
+            screen.innerText = "";
+        }
+ 
+        else{
+            screen.innerText += e.value;
+        }
     })
 })
+
+
+function add(val1, val2){
+    return val1 + val2;
+}
+function sub(val1, val2){
+    return val1 - val2;
+}
+function mult(val1, val2){
+    return val1 - val2;
+}
+function div(val1, val2){
+    if(val2 === 0){
+        return "zero division not possible"
+    }
+    return val1/val2;
+}
 
 
 
